c50ac551a6bd9a6f4d62eb6c00604c4959cc7f01
Juan Rickert <jerickert@gmail.com>
2023-11-30 19:11:14 -0300

    Eliminacion de superposicion de datos 2023/11/30

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if (navigator.serviceWorker) {   
    navigator.serviceWorker.register("../sw.js");
-    console.log('En app.js, Service worker registrado '); 
+   console.log('En app.js, Service worker registrado ');  
 } else {console.log('NO podemos usar el service worker');}
 
 /* if (window.caches) {
--- js/validarExistenciatoken.js
+++ js/validarExistenciatoken.js
@@ -5,68 +5,73 @@ var tokenConComillas = token;
 var baererToken = "Bearer " + tokenSinComillas;
 
 if (token !== null && token !== undefined) {
-  console.log("El token está guardado en el localStorage:", token);
-  var tokenConComillas = token;
-  var tokenSinComillas = tokenConComillas.replace(/^"|"$/g, "");
-  console.log(tokenSinComillas);
-  var baererToken = "Bearer " + tokenSinComillas;
-  console.log("baererToken: ", baererToken);
-  var url = "https://ospif.intelligentia.com.ar" + "/api/v1/affiliates";
+    console.log("El token está guardado en el localStorage:", token);
+    var tokenConComillas = token;
+    var tokenSinComillas = tokenConComillas.replace(/^"|"$/g, "");
+    console.log(tokenSinComillas);
+    var baererToken = "Bearer " + tokenSinComillas;
+    console.log("baererToken: ", baererToken);
+    var url = "https://ospif.intelligentia.com.ar" + "/api/v1/affiliates";
 
-  var opciones = {
-    method: "GET",
-    headers: {
-      Authorization: baererToken,
-    },
-  };
+    var opciones = {
+        method: "GET",
+        headers: {
+            Authorization: baererToken,
+        },
+    };
 
-  
- /*  console.log("url index: ", url);
+    /*  console.log("url index: ", url);
   console.log("opciones index: ", opciones); */
-  fetch(url, opciones)
-    .then(function (response) {
-      console.log("statusCode: ", response.statusCode);
-      console.log("status: ", response.status);
-      if (response.status === 200) {
-        console.log(response.json());
-        var url = "./pages/credencial.html";
-        window.location.href = url;
-      } else {
-        // La solicitud no fue exitosa, maneja el error según corresponda
-        console.log(
-          "en la linea 60 del index, el error es: ",
-          response.statusText
-        );
-        console.log(
-          "en la linea 60 del index, el codig error es: ",
-          response.statusCode
-        );
-        console.log("response.json: ", response.json());
-        console.error("Error en la solicitud:", response.statusText);
-        console.error("TOKEN VENCIDO, OBRO LOGIN");
-         
-         window.location.href = "../pages/login.html";
-      }
-    })
+    fetch(url, opciones)
+        .then(function (response) {
+            console.log("statusCode: ", response.statusCode);
+            console.log("status: ", response.status);
+            if (response.status === 200) {
+                console.log(response.json());
+                var url = "./pages/credencial.html";
+                window.location.href = url;
+            } else {
+                // La solicitud no fue exitosa, maneja el error según corresponda
+                console.log(
+                    "en la linea 60 del index, el error es: ",
+                    response.statusText
+                );
+                console.log(
+                    "en la linea 60 del index, el codig error es: ",
+                    response.statusCode
+                );
+                console.log("response.json: ", response.json());
+                console.error("Error en la solicitud:", response.statusText);
+                localStorage.removeItem("token");
+                localStorage.removeItem("afiliado");
+                localStorage.removeItem("fechatoken");
+                localStorage.removeItem("grupo");
+                localStorage.removeItem("usuario");
+                console.error("TOKEN VENCIDO, LIMPIO LOCAL");
+                console.error("TOKEN VENCIDO, ABRO LOGIN");
 
-    .catch(function (error) {
-      // Maneja los errores de la solicitud aquí
-      console.error("Error:", error);
-      console.log(
-        "en la linea 60 del index, el error es: ",
-        response.statusText
-      );
-      console.log(
-        "en la linea 60 del index, el codig error es: ",
-        response.statusCode
-      );
-      console.log("response.json: ", response.json());
-      console.error("Error en la solicitud:", response.statusText);
-    });
+                window.location.href = "../pages/login.html";
+            }
+        })
+
+        .catch(function (error) {
+            // Maneja los errores de la solicitud aquí
+            console.error("Error:", error);
+            console.log(
+                "en la linea 60 del index, el error es: ",
+                response.statusText
+            );
+            console.log(
+                "en la linea 60 del index, el codig error es: ",
+                response.statusCode
+            );
+            console.log("response.json: ", response.json());
+            console.error("Error en la solicitud:", response.statusText);
+        });
 } else {
-  console.log("El token no está guardado en el localStorage.");
-  // Puedes manejar la situación cuando el token no está presente
+    console.log("El token no está guardado en el localStorage.");
+    // Puedes manejar la situación cuando el token no está presente
 
-  var url = "../pages/login.html";
-  window.location.href = url;
+    var url = "../pages/login.html";
+    window.location.href = url;
 }
